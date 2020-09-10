b0f56a35d43d35b07be6a534677378628ea14e73
daveringu <david.ringuet@polymtl.ca>
2020-09-10 22:13:29 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    distance_entre_min_et_max=ord()
+    distance_entre_min_et_max=ord()``'a'
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
