eee7b9358977927f37100a9421063fb9e80e9f49
daveringu <david.ringuet@polymtl.ca>
2020-09-10 22:12:43 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    distance_
+    distance_entre_min_et_max
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
