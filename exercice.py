bc30c9a4df57af384583ced1092391e41e6c657d
daveringu <david.ringuet@polymtl.ca>
2020-09-10 22:10:08 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@ def majuscule(mot):
     for lettre in mot:
         # TODO completer la fonction ici
         lettre = ord(lettre) -32
-        resultat += chr(lettre
+        resultat += cho(lettre)
     return resultat
 
 
